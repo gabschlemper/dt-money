e4aebfca5ecfdb5ef9e69ab45f2d05efca9bc048
Gabriela Schlemper <gabschlemper@gmail.com>
2023-10-27 21:21:41 -0300

    initial setup

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../Components/Header";
+import { Summary } from "../../Components/Summary";
+
+export function Transactions() {
+  return (
+    <>
+      <Header />
+      <Summary />
+    </>
+  );
+}
